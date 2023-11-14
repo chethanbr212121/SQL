3a2f35625695b29ee97111cffd1e316491b131f9
chethanbr212121 <chethanbr212121@gmail.com>
2023-11-14 20:30:35 +0530

    sql first code

---
+++ ADMIN1.sql
@@ -0,0 +1,31 @@
+create user etl7 identified by etl7;
+
+grant create session to etl7
+
+grant create table to etl7
+
+grant resource to etl7
+
+grant dba to etl7
+
+
+
+grant create session to CHETU
+
+grant create table to  CHETU
+
+grant resource to  CHETU
+
+grant dba to  CHETU
+
+
+
+alter user hr account unlock;
+
+alter user hr identified by hr;
+
+
+
+
+
+COMMIT
